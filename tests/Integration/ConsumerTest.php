3a719b7ca78d0c766fe5f9b4fcdfc70b144abea3
Mario Blažek <blazek.mario@gmail.com>
2020-03-26 20:35:38 +0100

    Fixed CS tests

--- tests/Integration/ConsumerTest.php
+++ tests/Integration/ConsumerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Marek\Covid19\Tests\Integration;
 
 use Marek\Covid19\API\Cache\HandlerInterface;
@@ -23,7 +25,7 @@ class ConsumerTest extends TestCase
 
     protected $cacheHandler;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         $this->httpClient = $this->createMock(HttpClientInterface::class);
         $this->cacheHandler = $this->createMock(HandlerInterface::class);
@@ -35,9 +37,10 @@ class ConsumerTest extends TestCase
             (new DenormalizerFactory())->create()
         );
     }
+
     public function testSummary()
     {
-        $this->cacheHandler->expects($this->once())
+        $this->cacheHandler->expects(self::once())
             ->method('has')
             ->with('https://api.covid19api.com/summary')
             ->willReturn(false);
@@ -46,39 +49,39 @@ class ConsumerTest extends TestCase
 
         $response = new JsonResponse($json, 200);
 
-        $this->httpClient->expects($this->once())
+        $this->httpClient->expects(self::once())
             ->method('get')
             ->with('https://api.covid19api.com/summary')
             ->willReturn($response);
 
         $summary = $this->consumer->getSummary();
 
-        $this->assertCount(2, $summary->getSummaries());
+        self::assertCount(2, $summary->getSummaries());
 
         $data = json_decode($json, true);
 
-        $this->assertEquals($data['Countries'][0]['Country'], $summary->getSummaries()[0]->country);
-        $this->assertEquals($data['Countries'][0]['Slug'], $summary->getSummaries()[0]->slug);
-        $this->assertEquals($data['Countries'][0]['NewConfirmed'], $summary->getSummaries()[0]->newConfirmed);
-        $this->assertEquals($data['Countries'][0]['TotalConfirmed'], $summary->getSummaries()[0]->totalConfirmed);
-        $this->assertEquals($data['Countries'][0]['NewDeaths'], $summary->getSummaries()[0]->newDeaths);
-        $this->assertEquals($data['Countries'][0]['TotalDeaths'], $summary->getSummaries()[0]->totalDeaths);
-        $this->assertEquals($data['Countries'][0]['NewRecovered'], $summary->getSummaries()[0]->newDeaths);
-        $this->assertEquals($data['Countries'][0]['TotalRecovered'], $summary->getSummaries()[0]->totalRecovered);
+        self::assertSame($data['Countries'][0]['Country'], $summary->getSummaries()[0]->country);
+        self::assertSame($data['Countries'][0]['Slug'], $summary->getSummaries()[0]->slug);
+        self::assertSame($data['Countries'][0]['NewConfirmed'], $summary->getSummaries()[0]->newConfirmed);
+        self::assertSame($data['Countries'][0]['TotalConfirmed'], $summary->getSummaries()[0]->totalConfirmed);
+        self::assertSame($data['Countries'][0]['NewDeaths'], $summary->getSummaries()[0]->newDeaths);
+        self::assertSame($data['Countries'][0]['TotalDeaths'], $summary->getSummaries()[0]->totalDeaths);
+        self::assertSame($data['Countries'][0]['NewRecovered'], $summary->getSummaries()[0]->newDeaths);
+        self::assertSame($data['Countries'][0]['TotalRecovered'], $summary->getSummaries()[0]->totalRecovered);
 
-        $this->assertEquals($data['Countries'][1]['Country'], $summary->getSummaries()[1]->country);
-        $this->assertEquals($data['Countries'][1]['Slug'], $summary->getSummaries()[1]->slug);
-        $this->assertEquals($data['Countries'][1]['NewConfirmed'], $summary->getSummaries()[1]->newConfirmed);
-        $this->assertEquals($data['Countries'][1]['TotalConfirmed'], $summary->getSummaries()[1]->totalConfirmed);
-        $this->assertEquals($data['Countries'][1]['NewDeaths'], $summary->getSummaries()[1]->newDeaths);
-        $this->assertEquals($data['Countries'][1]['TotalDeaths'], $summary->getSummaries()[1]->totalDeaths);
-        $this->assertEquals($data['Countries'][1]['NewRecovered'], $summary->getSummaries()[1]->newDeaths);
-        $this->assertEquals($data['Countries'][1]['TotalRecovered'], $summary->getSummaries()[1]->totalRecovered);
+        self::assertSame($data['Countries'][1]['Country'], $summary->getSummaries()[1]->country);
+        self::assertSame($data['Countries'][1]['Slug'], $summary->getSummaries()[1]->slug);
+        self::assertSame($data['Countries'][1]['NewConfirmed'], $summary->getSummaries()[1]->newConfirmed);
+        self::assertSame($data['Countries'][1]['TotalConfirmed'], $summary->getSummaries()[1]->totalConfirmed);
+        self::assertSame($data['Countries'][1]['NewDeaths'], $summary->getSummaries()[1]->newDeaths);
+        self::assertSame($data['Countries'][1]['TotalDeaths'], $summary->getSummaries()[1]->totalDeaths);
+        self::assertSame($data['Countries'][1]['NewRecovered'], $summary->getSummaries()[1]->newDeaths);
+        self::assertSame($data['Countries'][1]['TotalRecovered'], $summary->getSummaries()[1]->totalRecovered);
     }
 
     public function testCountries()
     {
-        $this->cacheHandler->expects($this->once())
+        $this->cacheHandler->expects(self::once())
             ->method('has')
             ->with('https://api.covid19api.com/countries')
             ->willReturn(false);
@@ -87,29 +90,29 @@ class ConsumerTest extends TestCase
 
         $response = new JsonResponse($json, 200);
 
-        $this->httpClient->expects($this->once())
+        $this->httpClient->expects(self::once())
             ->method('get')
             ->with('https://api.covid19api.com/countries')
             ->willReturn($response);
 
         $countries = $this->consumer->getCountries();
 
-        $this->assertCount(2, $countries->getCountries());
+        self::assertCount(2, $countries->getCountries());
 
         $data = json_decode($json, true);
 
-        $this->assertEquals($data[0]['Country'], $countries->getCountries()[0]->country);
-        $this->assertEquals($data[0]['Slug'], $countries->getCountries()[0]->slug);
-        $this->assertEquals($data[0]['Provinces'], $countries->getCountries()[0]->provinces);
+        self::assertSame($data[0]['Country'], $countries->getCountries()[0]->country);
+        self::assertSame($data[0]['Slug'], $countries->getCountries()[0]->slug);
+        self::assertSame($data[0]['Provinces'], $countries->getCountries()[0]->provinces);
 
-        $this->assertEquals($data[1]['Country'], $countries->getCountries()[1]->country);
-        $this->assertEquals($data[1]['Slug'], $countries->getCountries()[1]->slug);
-        $this->assertEquals($data[1]['Provinces'], $countries->getCountries()[1]->provinces);
+        self::assertSame($data[1]['Country'], $countries->getCountries()[1]->country);
+        self::assertSame($data[1]['Slug'], $countries->getCountries()[1]->slug);
+        self::assertSame($data[1]['Provinces'], $countries->getCountries()[1]->provinces);
     }
 
     public function testStatistics()
     {
-        $this->cacheHandler->expects($this->once())
+        $this->cacheHandler->expects(self::once())
             ->method('has')
             ->with('https://api.covid19api.com/stats')
             ->willReturn(false);
@@ -118,52 +121,55 @@ class ConsumerTest extends TestCase
 
         $response = new JsonResponse($json, 200);
 
-        $this->httpClient->expects($this->once())
+        $this->httpClient->expects(self::once())
             ->method('get')
             ->with('https://api.covid19api.com/stats')
             ->willReturn($response);
 
-        $statistics = $this->consumer->getStatistics();;
+        $statistics = $this->consumer->getStatistics();
 
         $data = json_decode($json, true);
 
-        $this->assertEquals($data['Total'], $statistics->total);
-        $this->assertEquals($data['All'], $statistics->all);
-        $this->assertEquals($data['AllUpdated'], $statistics->allUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['Countries'], $statistics->countries);
-        $this->assertEquals($data['CountriesUpdated'], $statistics->countriesUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['ByCountry'], $statistics->byCountry);
-        $this->assertEquals($data['ByCountryUpdated'], $statistics->byCountryUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['ByCountryLive'], $statistics->byCountryLive);
-        $this->assertEquals($data['ByCountryLiveUpdated'], $statistics->byCountryLiveUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['ByCountryTotal'], $statistics->byCountryTotal);
-        $this->assertEquals($data['ByCountryTotalUpdated'], $statistics->byCountryTotalUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['DayOne'], $statistics->dayOne);
-        $this->assertEquals($data['DayOneUpdated'], $statistics->dayOneUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['DayOneLive'], $statistics->dayOneLive);
-        $this->assertEquals($data['DayOneLiveUpdated'], $statistics->dayOneLiveUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['DayOneTotal'], $statistics->dayOneTotal);
-        $this->assertEquals($data['DayOneTotalUpdated'], $statistics->dayOneTotalUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['LiveCountryStatus'], $statistics->liveCountryStatus);
-        $this->assertEquals($data['LiveCountryStatusUpdated'], $statistics->liveCountryStatusUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['LiveCountryStatusAfterDate'], $statistics->liveCountryStatusAfterDate);
-        $this->assertEquals($data['LiveCountryStatusAfterDateUpdated'], $statistics->liveCountryStatusAfterDateUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['Stats'], $statistics->statistics);
-        $this->assertEquals($data['StatsUpdated'], $statistics->statisticsUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['Default'], $statistics->default);
-        $this->assertEquals($data['DefaultUpdated'], $statistics->defaultUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['SubmitWebhook'], $statistics->submitWebhook);
-        $this->assertEquals($data['SubmitWebhookUpdated'], $statistics->submitWebhookUpdated->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data['Summary'], $statistics->summary);
-        $this->assertEquals($data['SummaryUpdated'], $statistics->summaryUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['Total'], $statistics->total);
+        self::assertSame($data['All'], $statistics->all);
+        self::assertSame($data['AllUpdated'], $statistics->allUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['Countries'], $statistics->countries);
+        self::assertSame($data['CountriesUpdated'], $statistics->countriesUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['ByCountry'], $statistics->byCountry);
+        self::assertSame($data['ByCountryUpdated'], $statistics->byCountryUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['ByCountryLive'], $statistics->byCountryLive);
+        self::assertSame($data['ByCountryLiveUpdated'], $statistics->byCountryLiveUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['ByCountryTotal'], $statistics->byCountryTotal);
+        self::assertSame($data['ByCountryTotalUpdated'], $statistics->byCountryTotalUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['DayOne'], $statistics->dayOne);
+        self::assertSame($data['DayOneUpdated'], $statistics->dayOneUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['DayOneLive'], $statistics->dayOneLive);
+        self::assertSame($data['DayOneLiveUpdated'], $statistics->dayOneLiveUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['DayOneTotal'], $statistics->dayOneTotal);
+        self::assertSame($data['DayOneTotalUpdated'], $statistics->dayOneTotalUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['LiveCountryStatus'], $statistics->liveCountryStatus);
+        self::assertSame($data['LiveCountryStatusUpdated'], $statistics->liveCountryStatusUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['LiveCountryStatusAfterDate'], $statistics->liveCountryStatusAfterDate);
+        self::assertSame($data['LiveCountryStatusAfterDateUpdated'], $statistics->liveCountryStatusAfterDateUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['Stats'], $statistics->statistics);
+        self::assertSame($data['StatsUpdated'], $statistics->statisticsUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['Default'], $statistics->default);
+        self::assertSame($data['DefaultUpdated'], $statistics->defaultUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['SubmitWebhook'], $statistics->submitWebhook);
+        self::assertSame($data['SubmitWebhookUpdated'], $statistics->submitWebhookUpdated->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data['Summary'], $statistics->summary);
+        self::assertSame($data['SummaryUpdated'], $statistics->summaryUpdated->format('Y-m-d\TH:i:s\Z'));
     }
 
     /**
      * @dataProvider reports
+     *
+     * @param mixed $url
+     * @param mixed $method
      */
     public function testReports($url, $method)
     {
-        $this->cacheHandler->expects($this->once())
+        $this->cacheHandler->expects(self::once())
             ->method('has')
             ->with($url)
             ->willReturn(false);
@@ -172,7 +178,7 @@ class ConsumerTest extends TestCase
 
         $response = new JsonResponse($json, 200);
 
-        $this->httpClient->expects($this->once())
+        $this->httpClient->expects(self::once())
             ->method('get')
             ->with($url)
             ->willReturn($response);
@@ -180,19 +186,19 @@ class ConsumerTest extends TestCase
         $country = new Country(Countries::COUNTRY_UNITED_KINGDOM);
         $status = new Status(Cases::CONFIRMED);
 
-        $report = $this->consumer->$method($country, $status);
+        $report = $this->consumer->{$method}($country, $status);
 
         $data = json_decode($json, true);
 
-        $this->assertCount(1, $report->getReports());
+        self::assertCount(1, $report->getReports());
 
-        $this->assertEquals($data[0]['Country'], $report->getReports()[0]->country);
-        $this->assertEquals($data[0]['Province'], $report->getReports()[0]->province);
-        $this->assertEquals($data[0]['Lat'], $report->getReports()[0]->latitude);
-        $this->assertEquals($data[0]['Lon'], $report->getReports()[0]->longitude);
-        $this->assertEquals($data[0]['Date'], $report->getReports()[0]->date->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data[0]['Cases'], $report->getReports()[0]->cases);
-        $this->assertEquals($data[0]['Status'], $report->getReports()[0]->status);
+        self::assertSame($data[0]['Country'], $report->getReports()[0]->country);
+        self::assertSame($data[0]['Province'], $report->getReports()[0]->province);
+        self::assertSame($data[0]['Lat'], $report->getReports()[0]->latitude);
+        self::assertSame($data[0]['Lon'], $report->getReports()[0]->longitude);
+        self::assertSame($data[0]['Date'], $report->getReports()[0]->date->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data[0]['Cases'], $report->getReports()[0]->cases);
+        self::assertSame($data[0]['Status'], $report->getReports()[0]->status);
     }
 
     public function reports(): array
@@ -234,7 +240,7 @@ class ConsumerTest extends TestCase
         $dateTime = new \DateTimeImmutable('now');
         $url = 'https://api.covid19api.com/live/country/united-kingdom/status/confirmed/date/' . $dateTime->format('Y-m-d\TH:i:s\Z');
 
-        $this->cacheHandler->expects($this->once())
+        $this->cacheHandler->expects(self::once())
             ->method('has')
             ->with($url)
             ->willReturn(false);
@@ -243,7 +249,7 @@ class ConsumerTest extends TestCase
 
         $response = new JsonResponse($json, 200);
 
-        $this->httpClient->expects($this->once())
+        $this->httpClient->expects(self::once())
             ->method('get')
             ->with($url)
             ->willReturn($response);
@@ -256,22 +262,22 @@ class ConsumerTest extends TestCase
 
         $data = json_decode($json, true);
 
-        $this->assertCount(1, $report->getReports());
+        self::assertCount(1, $report->getReports());
 
-        $this->assertEquals($data[0]['Country'], $report->getReports()[0]->country);
-        $this->assertEquals($data[0]['Province'], $report->getReports()[0]->province);
-        $this->assertEquals($data[0]['Lat'], $report->getReports()[0]->latitude);
-        $this->assertEquals($data[0]['Lon'], $report->getReports()[0]->longitude);
-        $this->assertEquals($data[0]['Date'], $report->getReports()[0]->date->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data[0]['Cases'], $report->getReports()[0]->cases);
-        $this->assertEquals($data[0]['Status'], $report->getReports()[0]->status);
+        self::assertSame($data[0]['Country'], $report->getReports()[0]->country);
+        self::assertSame($data[0]['Province'], $report->getReports()[0]->province);
+        self::assertSame($data[0]['Lat'], $report->getReports()[0]->latitude);
+        self::assertSame($data[0]['Lon'], $report->getReports()[0]->longitude);
+        self::assertSame($data[0]['Date'], $report->getReports()[0]->date->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data[0]['Cases'], $report->getReports()[0]->cases);
+        self::assertSame($data[0]['Status'], $report->getReports()[0]->status);
     }
 
     public function testGetAllData()
     {
         $url = 'https://api.covid19api.com/all';
 
-        $this->cacheHandler->expects($this->once())
+        $this->cacheHandler->expects(self::once())
             ->method('has')
             ->with($url)
             ->willReturn(false);
@@ -280,7 +286,7 @@ class ConsumerTest extends TestCase
 
         $response = new JsonResponse($json, 200);
 
-        $this->httpClient->expects($this->once())
+        $this->httpClient->expects(self::once())
             ->method('get')
             ->with($url)
             ->willReturn($response);
@@ -289,14 +295,14 @@ class ConsumerTest extends TestCase
 
         $data = json_decode($json, true);
 
-        $this->assertCount(1, $report->getReports());
+        self::assertCount(1, $report->getReports());
 
-        $this->assertEquals($data[0]['Country'], $report->getReports()[0]->country);
-        $this->assertEquals($data[0]['Province'], $report->getReports()[0]->province);
-        $this->assertEquals($data[0]['Lat'], $report->getReports()[0]->latitude);
-        $this->assertEquals($data[0]['Lon'], $report->getReports()[0]->longitude);
-        $this->assertEquals($data[0]['Date'], $report->getReports()[0]->date->format('Y-m-d\TH:i:s\Z'));
-        $this->assertEquals($data[0]['Cases'], $report->getReports()[0]->cases);
-        $this->assertEquals($data[0]['Status'], $report->getReports()[0]->status);
+        self::assertSame($data[0]['Country'], $report->getReports()[0]->country);
+        self::assertSame($data[0]['Province'], $report->getReports()[0]->province);
+        self::assertSame($data[0]['Lat'], $report->getReports()[0]->latitude);
+        self::assertSame($data[0]['Lon'], $report->getReports()[0]->longitude);
+        self::assertSame($data[0]['Date'], $report->getReports()[0]->date->format('Y-m-d\TH:i:s\Z'));
+        self::assertSame($data[0]['Cases'], $report->getReports()[0]->cases);
+        self::assertSame($data[0]['Status'], $report->getReports()[0]->status);
     }
 }
